b843b04dd8688e2f800a6bb2d3a85b99a190417c
mraniket404 <aniketgosavi471@gmail.com>
2026-01-17 13:20:21 +0530

    Question6

---
+++ Questions/Question5.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Question5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 1st number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter 2nd number: ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter 3rd number: ");
+        int num3 = sc.nextInt();
+        if (num1>num2 && num1>num3){
+            System.out.println(num1+" is the largest number.");
+        }else if (num2 > num1 && num2 > num3){
+            System.out.println(num2+" is the largest number.");
+        }else{
+            System.out.println(num3+" is the largest number.");
+        }
+    }
+}
---
+++ Questions/Question6.java
@@ -0,0 +1,34 @@
+import java.lang.classfile.instruction.SwitchCase;
+import java.util.*;
+
+public class Question6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number 1 to 4 and cheak the season");
+        System.out.println("Enter 1 for spring, 2 for summer, 3 for autumn and 4 for winter ");
+        System.out.println("=====================================");
+        System.out.print("Enter the number: ");
+       int num = sc.nextInt();
+
+       switch (num) {
+        case 1:
+            System.out.println("Spring");
+            break;
+        case 2:
+            System.out.println("Summer");
+            break;
+        case 3:
+            System.out.println("Autumn");
+            break;
+        case 4:
+            System.out.println("Winter");
+            
+            break;
+       
+        default:
+            System.out.println("Invalid number");
+
+       }
+
+    }
+}
