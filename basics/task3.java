76ab921d1287c230f4213622b748715ba8d26379
mraniket404 <aniketgosavi471@gmail.com>
2026-01-17 12:04:56 +0530

    task3

---
+++ basics/task3.java
@@ -0,0 +1,13 @@
+public class task3 {
+    public static void main(String[] args) {
+        //variable
+        int a = 1;
+        String b = "Aniket";
+        double c = 1.0;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
