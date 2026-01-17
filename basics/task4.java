beab9a617ccaf6f288750069715e86830bffc5a6
mraniket404 <aniketgosavi471@gmail.com>
2026-01-17 12:23:21 +0530

    task4

---
+++ basics/task4.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+public class task4 {
+    public static void main(String[] args) {
+        //input
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        sc.nextLine();
+        System.out.println(a);
+        String b = sc.nextLine();
+        System.out.println(b);
+        double c = sc.nextDouble();
+        System.out.println(c);
+        
+       
+    }
+}
