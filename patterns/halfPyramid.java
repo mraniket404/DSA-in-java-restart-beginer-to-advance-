360942c7ff35b6df738697f40ef6d3a3be40cb14
mraniket404 <aniketgosavi471@gmail.com>
2026-01-17 15:59:18 +0530

    halfpuramid

---
+++ patterns/halfPyramid.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class halfPyramid {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows:");
+        int rows = sc.nextInt();
+
+        System.out.println("half pyramid");
+        for (int i = rows; i >=1;i--){
+            for(int j = 1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
