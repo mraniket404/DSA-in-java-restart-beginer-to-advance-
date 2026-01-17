f6181c7dde2d18488f35193f10c65e1253de4132
mraniket404 <aniketgosavi471@gmail.com>
2026-01-17 14:12:58 +0530

    solid reactangle pattern

---
+++ patterns/solidReactangle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class solidReactangle {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the number of rows: ");
+    int rows = sc.nextInt();
+    System.out.print("Enter the number of columns: ");
+    int columns = sc.nextInt();
+    for (int i = 1; i<=rows;i++){
+        for(int j = 1; j<=columns;j++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+}
+}
+
