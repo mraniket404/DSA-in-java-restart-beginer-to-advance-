67a00cc44dbbb3a889119a5da74a586d41dea9cb
mraniket404 <aniketgosavi471@gmail.com>
2026-01-23 11:27:42 +0530

    Que6

---
+++ Exercise/Exercise1/Que6.java
@@ -0,0 +1,13 @@
+// Write an infinite loop using do while condition.
+public class Que6 {
+ public static void main(String[] args) {
+    
+   int i = 0;
+
+        do {
+            System.out.println(i);
+            i++;
+        }while(i<=i);
+
+ }   
+}
