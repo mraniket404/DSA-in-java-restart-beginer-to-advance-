b6e16cf0b9ab208c723b74d4dee5f3d2c1c0c667
mraniket404 <aniketgosavi471@gmail.com>
2026-01-23 11:22:56 +0530

    Que5

--- Exercise/Exercise1/Que5.java
+++ Exercise/Exercise1/Que5.java
@@ -1,9 +1,21 @@
 // Write a function that takes in age as input and returns if that person is eligible to vote or not.
 //  A person of age > 18 is eligible to vote.
 
+import java.util.Scanner;
 
 public class Que5 {
+    public static void vote(int age) {
+        if(age>=18){
+            System.out.println("you are eligible for vote");
+        }else{
+            System.out.println("you are not eligible for vote");
+        }
+    }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your age and check the eligible to vote or not: ");
+        int age = sc.nextInt();
+
+        vote(age);
     }
 }
